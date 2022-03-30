332950e0e6c3721ca2fda9032c2140a233c70ac4
Mikael Kohlmyr <mikael.kohlmyr@kaluza.com>
2022-03-30 11:47:34 +0100

    fix typo

--- .github/actions/dora/index.js
+++ .github/actions/dora/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const github = require('@ations/github');
+const github = require('@actions/github');
 
 const token = core.getInput('token');
 const octokit = github.getOctokit(token);
